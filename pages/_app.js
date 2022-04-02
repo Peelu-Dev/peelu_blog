5a045e2c0153a80321f28dae8cc981f8f4a5062e
peelu123 <raghuwanshipalash6@gmail.com>
2022-04-02 17:36:41 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import react,{useEffect,useState} from 'react';
+import { Layout } from '../components';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss'
+import Script from 'next/script';
 
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-KD0VM5DJ1B"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-KD0VM5DJ1B');
+// </script>
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+    <Layout>
+       <Component {...pageProps} />
+    </Layout>
+    </>
+  ) 
+
 }
 
 export default MyApp
